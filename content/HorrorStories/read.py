9633df51cdcbfcff0e37697dc34787e4e6a1a606
SuperThunder <alexander.wolfe.10@googlemail.com>
2017-06-14 23:06:18 -0400

    partway through writing next iteration up of program

--- content/HorrorStories/read.py
+++ content/HorrorStories/read.py
@@ -1,10 +1,36 @@
-with open("horror.txt", 'r') as horrorfile:
-    filecont = horrorfile.read()
-    stories = filecont.split('-'*77)
-    print("Stories: %d"%len(stories))
+def main():
+	
+	import time # for sleeping - waiting without busy loops
+	#import threading # for later
+	inputqueue = []
+	CurrentStory = 0
+	
+	# Main control loop - handles showing text
+	def control():
+		import os # for clearing the console
+		stories = []
+		with open("horror.txt", 'r') as horrorfile:
+			content = horrorfile.read()
+			stories = content.split('-'*77)
+			print("Stories: %d\n"%len(stories))
+			
+		
+		while True:
+			inputqueue.pop(0)
+			# f: move forward a story
+			
+			# b: move backward
+			
+			# else: input error
+		
+	
+	# Input function
+	def input():
+		print("\nCommand: ",)
+		inputqueue.append( input() )
 
-import os
-for story in stories:
-    print(story)
-    os.system('clear')
-    input()
+
+	control()
+
+
+main()
